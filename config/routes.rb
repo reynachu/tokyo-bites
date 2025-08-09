3205ced29bd1f1c891d26c5dc6231bf154c65505
Sunny Jayswal <sunnyjuw@gmail.com>
2025-08-09 11:12:02 +0900

    restaurant controller and preliminary views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'restaurants/index'
+  get 'restaurants/show'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
