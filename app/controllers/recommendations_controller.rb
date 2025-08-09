71b99abe02df0d7682af7e28a16834b54a37b3ef
WIWIEK ANDRIANI <andrianiwiwiek753@gmail.com>
2025-08-09 11:12:04 +0900

    update recommendation controller

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,9 +1,26 @@
 class RecommendationsController < ApplicationController
   def index
-    @recommendations = Recommendation.all
+    @recommendations = @restaurant.recommendations
   end
 
+  def new
+    @recommendation = @restaurants.recomendations.build
+  end
+
+  def create
+    @recommendation = @restaurant.recommendations.build(recommendation_params)
+    @recommendation.user = current_user
+
+    if @recommendation.save
+      redirect_to restaurant_recomendations_path(@restaurant), notice: "Recommendation created successfully"
+    else
+      render :new
+    end
+  end
+
+  private
+
   def recommendation_params
-    params.require(:recomendation).permit(:description, :photos, :restaurant_tags)
+    params.require(:recomendation).permit(:description, :restaurant_tags)
   end
 end
