fece5a634703de38db0f8982e7e4f8054d675ccb
Mizuki Harthan <mizharthan@gmail.com>
2025-08-20 03:03:37 +0900

    fixed markers grouping

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_08_12_102701) do
+ActiveRecord::Schema[7.1].define(version: 2025_08_19_163508) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -93,6 +93,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_12_102701) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+    t.string "address_jp"
   end
 
   create_table "users", force: :cascade do |t|
