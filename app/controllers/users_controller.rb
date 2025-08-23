d2b159d6cbababcbbd619998c9d9b4d0588ef843
WIWIEK ANDRIANI <andrianiwiwiek753@gmail.com>
2025-08-23 18:01:35 +0900

    bookmark

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,26 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_user, only: [:show, :wishlist]
   skip_after_action :verify_authorized, only: [:profile, :show]
 
   def profile
     @user = current_user
   end
+
+  # GET /users
+  def index
+    @users = policy_scope(User)
+
+    if params[:q].present?
+      @users = @users.where("username ILIKE ?", "%#{params[:q]}%")
+    end
+
+    respond_to do |format|
+      format.html
+      format.turbo_stream { render partial: "users/friend_search_results", locals: { users: @users } }
+    end
+  end
+
 
   def show
     @user = User.find(params[:id])
@@ -15,5 +31,16 @@ class UsersController < ApplicationController
     else
       render :show
     end
+  end
+
+  def wishlist
+   @user = User.find(params[:id])
+   @restaurants = policy_scope(@user.wishlist_restaurants)
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   get 'plans/index'
   get "map", to: "pages#map", as: :map
   get "profile", to: "users#profile", as: :profile
+  get "search", to: "search#index", as: :search
+
   devise_for :users
   root to: "pages#home"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -15,23 +18,34 @@ Rails.application.routes.draw do
 
   resources :restaurants, only: [:index, :show] do
     resources :recommendations, only: [:index, :new, :create]
-     collection do
+    collection do
       get :map
     end
+
+  # Bookmark routes (POST + DELETE only)
+    resource :bookmarks, only: [:show, :create, :destroy]
   end
+
+
+  resources :wishlists, only: [:index, :create, :destroy]   # 👈 THIS gives you wishlists_path
+
 
   # unnested recommendation resource
   resources :recommendations, only: [:index, :new, :create]
 
   # Global recommendations index (all restaurants)
-  resources :recommendations, only: [:index]
+  # resources :recommendations, only: [:index]
 
   # plans
   resources :plans, only: [:index]
 
-  #users
-  resources :users, only: [:show] do
+  # users
+  resources :users, only: [:show, :index] do
     post 'follow', to: 'socializations#follow', as: :follow
     delete 'unfollow', to: 'socializations#unfollow', as: :unfollow
+
+    member do
+      get :wishlist
+    end
   end
 end
